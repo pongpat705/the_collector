e34f17d44e621769ae127f9fb86f065200b3ce3e
fluke <fhluky@gmail.com>
2018-03-17 21:15:27 +0700

    first commit

---
+++ src/main/java/th/co/collector/TheCollectorApplication.java
@@ -0,0 +1,12 @@
+package th.co.collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCollectorApplication.class, args);
+	}
+}
---
+++ src/test/java/th/co/collector/TheCollectorApplicationTests.java
@@ -0,0 +1,16 @@
+package th.co.collector;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheCollectorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
